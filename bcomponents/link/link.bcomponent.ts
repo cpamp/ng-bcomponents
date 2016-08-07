f0a873eba63a95ee50d935e3d1ee0c1c89e875e6
Clint Pamperin <c.pamperin21@gmail.com>
2016-08-06 20:49:37 -0500

    add: link component

---
+++ bcomponents/link/link.bcomponent.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+import {BComponent, BComponentAttributes, BComponentInputs} from '../bcomponent';
+
+@Component({
+    selector: "link-bcomponent",
+    templateUrl: "link.bcomponent.html",
+    directives: [BComponentAttributes],
+    inputs: BComponentInputs.concat(['label', 'link'])
+})
+export class LinkBComponent extends BComponent {
+    label: string;
+    link: string;
+}
