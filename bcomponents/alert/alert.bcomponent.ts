0b9f278042333337fbb6223484538868ff775432
Clint Pamperin <c.pamperin21@gmail.com>
2016-08-07 18:54:19 -0500

    add: alert component

---
+++ bcomponents/alert/alert.bcomponent.ts
@@ -0,0 +1,36 @@
+import {Component, ViewChild} from '@angular/core';
+import {BComponent, BComponentAttributes, BComponentInputs, DisplayType} from '../bcomponent';
+
+@Component({
+    selector: "alert-bcomponent",
+    templateUrl: "alert.bcomponent.html",
+    directives: [BComponentAttributes],
+    inputs: BComponentInputs.concat(['text', 'dismissible', 'hidden', 'type'])
+})
+export class AlertBComponent extends BComponent {
+    @ViewChild(BComponentAttributes) attributes: BComponentAttributes;
+    public text: string;
+    public dismissible: boolean = false;
+    public hidden: boolean = false;
+    public type: DisplayType = "success";
+
+    constructor() {
+        super("alert alert-success");
+    }
+
+    ngOnChildChanges = () => {
+        this.type = this.type === "default" || this.type === "primary" ? "success" : this.type;
+        this.baseClass = "alert alert-" + this.type;
+        if(this.dismissible) {
+            this.baseClass += " alert-dismissible";
+        }
+    }
+
+    show() {
+        this.hidden = true;
+    }
+
+    hide() {
+        this.hidden = false;
+    }
+}
