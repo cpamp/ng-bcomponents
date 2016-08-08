5ceaba5665074ed8b32cebd7112bd74b489aa677
Clint Pamperin <c.pamperin21@gmail.com>
2016-08-07 22:32:27 -0500

    add: button bcomponent

---
+++ bcomponents/button/button.bcomponent.ts
@@ -0,0 +1,26 @@
+import {Component} from '@angular/core';
+import {BComponent, BComponentAttributes, BComponentInputs, DisplayType} from '../bcomponent';
+
+@Component({
+    selector: "button-bcomponent",
+    templateUrl: "button.bcomponent.html",
+    directives: [BComponentAttributes],
+    inputs: BComponentInputs.concat(['text', 'type', 'click'])
+})
+export class ButtonBComponent extends BComponent {
+    public value: number = 0;
+    public type: DisplayType = "default";
+    public click: () => void;
+
+    constructor() {
+        super("btn btn-default");
+    }
+
+    ngOnChildChanges = () => {
+        this.baseClass = "btn btn-" + this.type;
+    }
+
+    public onClick = () => {
+        if(this.click != null) this.click();
+    }
+}
