bcdd2c8c884785b81de1a3eebbef4a421beaa846
Clint Pamperin <c.pamperin21@gmail.com>
2016-08-21 00:08:47 -0500

    refactor: use link bcomponent

--- bcomponents/breadcrumb/breadcrumb.bcomponent.ts
+++ bcomponents/breadcrumb/breadcrumb.bcomponent.ts
@@ -2,16 +2,6 @@ import {Component} from '@angular/core';
 import {BComponent, BComponentAttributes, BComponentInputs} from '../bcomponent';
 import {LinkBComponent} from '../link/link.bcomponent';
 
-export class BreadcrumbItem {
-    public link: string;
-    public text: string;
-
-    constructor(link: string, text: string) {
-        this.link = link;
-        this.text = text;
-    }
-}
-
 @Component({
     selector: 'breadcrumb-bcomponent',
     templateUrl: 'breadcrumb.bcomponent.html',
@@ -19,7 +9,7 @@ export class BreadcrumbItem {
     inputs: BComponentInputs.concat('items', 'active')
 })
 export class BreadcrumbBComponent extends BComponent {
-    public items: BreadcrumbItem[];
+    public items: LinkBComponent[];
     public active: string;
 
     constructor() {
