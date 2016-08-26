54e0bf229de4f19b3d8963ff826cea93598528c3
Clint Pamperin <c.pamperin21@gmail.com>
2016-08-25 23:23:41 -0500

    fix: use better unique key

--- bcomponents/identifier.factory.ts
+++ bcomponents/identifier.factory.ts
@@ -1,28 +1,10 @@
 export class IdentifierFactory {
-    private static getKey = (): string => {
-        var key = new Date().getTime();
-        if(window != null && window.crypto != null && typeof window.crypto.getRandomValues === 'function') {
-            var array = new Uint32Array(1);
-            window.crypto.getRandomValues(array);
-            key += array[0];
-        } else {
-            key += Math.random();
-        }
-        return key.toString();
+    private static getToken = (): string => {
+        return Math.random().toString(36).substr(2);
     }
 
     public static getIdentifier = (): string => {
-        // 32 bit FNV-1a hash
-        // Ref.: http://isthe.com/chongo/tech/comp/fnv/
-        var key: string = IdentifierFactory.getKey();
-        var FNV1_32A_INIT = 0x811c9dc5;
-        var hval = FNV1_32A_INIT;
-        for ( var i = 0; i < key.length; ++i )
-        {
-            hval ^= key.charCodeAt(i);
-            hval += (hval << 1) + (hval << 4) + (hval << 7) + (hval << 8) + (hval << 24);
-        }
-        return (hval >>> 0).toString();
+        return IdentifierFactory.getToken() + IdentifierFactory.getToken();
     }
 
     public static getSelector = (identifier: string): string => {
