a8dbabbf777289dc72f1d34f1f38a7526e66fe92
Clint Pamperin <c.pamperin21@gmail.com>
2016-08-29 20:43:06 -0500

    add: router links

--- bcomponents/link/link.bcomponent.ts
+++ bcomponents/link/link.bcomponent.ts
@@ -5,19 +5,21 @@ import {BComponent, BComponentAttributes, BComponentInputs} from '../bcomponent'
     selector: "link-bcomponent",
     templateUrl: "link.bcomponent.html",
     directives: [BComponentAttributes],
-    inputs: BComponentInputs.concat(['text', 'link'])
+    inputs: BComponentInputs.concat(['text', 'link', 'router'])
 })
 export class LinkBComponent extends BComponent {
     public text: string;
     public link: string;
+    public router: boolean = false;
 
     constructor() {
         super(null);
     }
 
-    public Initialize = (text: string = "", link: string = ""): LinkBComponent => {
+    public Initialize = (text: string = "", link: string = "", router: boolean = false): LinkBComponent => {
         this.text = text;
         this.link = link;
+        this.router = router;
         if(this.ngOnChildChanges != null) this.ngOnChildChanges();
         return this;
     }
