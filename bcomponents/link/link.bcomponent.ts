d4e1538d3f6ff675c504f4c3495eba04334f4271
Clint Pamperin <c.pamperin21@gmail.com>
2016-09-05 22:07:18 -0500

    add: onClick and click event

--- bcomponents/link/link.bcomponent.ts
+++ bcomponents/link/link.bcomponent.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, Output, EventEmitter} from '@angular/core';
 import {BComponent, BComponentInputs} from '../bcomponent';
 
 @Component({
@@ -10,20 +10,29 @@ export class LinkBComponent extends BComponent {
     @Input() text: string;
     @Input() link: string;
     @Input() router: boolean = false;
+    @Input() onClick: () => void;
+
+    @Output() click: EventEmitter<LinkBComponent> = new EventEmitter<LinkBComponent>();
 
     constructor() {
         super(null);
     }
 
-    public Initialize = (text: string = "", link: string = "", router: boolean = false): LinkBComponent => {
+    public Initialize = (text: string = "", link: string = "", router: boolean = false, onClick: () => void = null): LinkBComponent => {
         this.text = text;
         this.link = link;
         this.router = router;
+        this.onClick = onClick;
         if(this.ngOnChildChanges != null) this.ngOnChildChanges();
         return this;
     }
 
     public hasText = (): boolean => {
         return !this.isNull(this.text);
+    }
+
+    public clickEvent = () => {
+        if(!this.isNull(this.onClick)) { this.onClick(); }
+        this.click.emit(this);
     }
 }
