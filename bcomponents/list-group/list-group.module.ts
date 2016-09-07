1152303b844765eeffdc9fe1901cd2b51463c9bf
Clint Pamperin <c.pamperin21@gmail.com>
2016-09-06 22:17:11 -0500

    add: list-item to list-group

--- bcomponents/list-group/list-group.module.ts
+++ bcomponents/list-group/list-group.module.ts
@@ -2,11 +2,12 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AttributesModule} from '../attributes.module';
 import {ListGroupBComponent} from './list-group.bcomponent';
+import {ListItemBComponent} from './list-item.bcomponent';
 import {BadgeModule} from '../badge/badge.module';
 
 @NgModule({
     imports: [BrowserModule, AttributesModule, BadgeModule],
-    declarations: [ListGroupBComponent],
+    declarations: [ListGroupBComponent, ListItemBComponent],
     exports: [ListGroupBComponent]
 })
 export class ListGroupModule {}
