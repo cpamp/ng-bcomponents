bf3f766bb8f454582d421fcb5137ea52f1d2820f
Clint Pamperin <c.pamperin21@gmail.com>
2016-09-08 23:55:45 -0500

    add: button group directive

--- bcomponents/button-group/button-group.bcomponent.ts
+++ bcomponents/button-group/button-group.bcomponent.ts
@@ -1,22 +1,17 @@
-import {Component, Input} from '@angular/core';
+import {Component, Directive, ElementRef, Input} from '@angular/core';
 import {BComponent, BComponentInputs, DisplaySize} from '../bcomponent';
 import {ButtonBComponent} from '../button/button.bcomponent';
 import {ComponentFactory} from '../component.factory';
 
-@Component({
-    selector: 'bc-button-group',
-    templateUrl: 'button-group.bcomponent.html',
-    inputs: BComponentInputs
-})
-export class ButtonGroupBComponent extends BComponent {
+export class ButtonGroupBase extends BComponent {
     @Input() items: ButtonBComponent[];
     @Input() size: DisplaySize;
 
-    constructor() {
-        super("btn-group");
+    constructor(el: ElementRef = void 0) {
+        super("btn-group", el);
     }
 
-    public Initialize = (items: ButtonBComponent[] = [], size: DisplaySize = null): ButtonGroupBComponent => {
+    public Initialize = (items: ButtonBComponent[] = [], size: DisplaySize = null): this => {
         this.items = items;
         if(this.ngOnChildChanges != null) this.ngOnChildChanges();
         this.items[0] = this.items[0].Initialize().InitializeAttributes()
@@ -28,4 +23,21 @@ export class ButtonGroupBComponent extends BComponent {
             this.baseClass = "btn-group btn-group-" + this.size;
         }
     }
+}
+
+@Component({
+    selector: 'bc-button-group',
+    templateUrl: 'button-group.bcomponent.html',
+    inputs: BComponentInputs
+})
+export class ButtonGroupBComponent extends ButtonGroupBase {
+    constructor() { super(); }
+}
+
+@Directive({
+    selector: '[bc-button-group]',
+    inputs: BComponentInputs
+})
+export class ButtonGroupBDirective extends ButtonGroupBase {
+    constructor(el: ElementRef) { super(el); }
 }
--- bcomponents/button-group/button-group.module.ts
+++ bcomponents/button-group/button-group.module.ts
@@ -1,12 +1,17 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AttributesModule} from '../attributes.module';
-import {ButtonGroupBComponent} from './button-group.bcomponent';
+import {ButtonGroupBComponent, ButtonGroupBDirective} from './button-group.bcomponent';
 import {ButtonModule} from '../button/button.module';
+
+var directives = [
+    ButtonGroupBComponent,
+    ButtonGroupBDirective
+];
 
 @NgModule({
     imports: [BrowserModule, AttributesModule, ButtonModule],
-    declarations: [ButtonGroupBComponent],
-    exports: [ButtonGroupBComponent]
+    declarations: directives,
+    exports: directives
 })
 export class ButtonGroupModule {}
