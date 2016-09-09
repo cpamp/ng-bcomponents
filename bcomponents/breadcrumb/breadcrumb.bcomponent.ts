3c8b54dda6042a5e8e060c24de6979a8cda5f7b9
Clint Pamperin <c.pamperin21@gmail.com>
2016-09-08 23:45:58 -0500

    add: breadcrumb directive

--- bcomponents/breadcrumb/breadcrumb.bcomponent.ts
+++ bcomponents/breadcrumb/breadcrumb.bcomponent.ts
@@ -1,24 +1,36 @@
-import {Component, Input} from '@angular/core';
+import {Component, Directive, Input, ElementRef} from '@angular/core';
 import {BComponent, BComponentInputs} from '../bcomponent';
 import {LinkBComponent} from '../link/link.bcomponent';
+
+export class BreadcrumbBase extends BComponent {
+    @Input() items: LinkBComponent[];
+    @Input() active: string;
+
+    constructor(el: ElementRef = void 0) {
+        super("breadcrumb");
+    }
+
+    public Initialize = (items: LinkBComponent[] = null, active: string = null): this => {
+        this.items = items;
+        this.active = active;
+        if(this.ngOnChildChanges != null) this.ngOnChildChanges();
+        return this;
+    }
+}
 
 @Component({
     selector: 'bc-breadcrumb',
     templateUrl: 'breadcrumb.bcomponent.html',
     inputs: BComponentInputs
 })
-export class BreadcrumbBComponent extends BComponent {
-    @Input() items: LinkBComponent[];
-    @Input() active: string;
+export class BreadcrumbBComponent extends BreadcrumbBase {
+    constructor() { super(); }
+}
 
-    constructor() {
-        super("breadcrumb");
-    }
-
-    public Initialize = (items: LinkBComponent[] = null, active: string = null): BreadcrumbBComponent => {
-        this.items = items;
-        this.active = active;
-        if(this.ngOnChildChanges != null) this.ngOnChildChanges();
-        return this;
-    }
+@Directive({
+    selector: '[bc-breadcrumb]',
+    inputs: BComponentInputs
+})
+export class BreadcrumbBDirective extends BreadcrumbBase {
+    constructor(el: ElementRef) { super(el); }
 }
--- bcomponents/breadcrumb/breadcrumb.module.ts
+++ bcomponents/breadcrumb/breadcrumb.module.ts
@@ -1,12 +1,17 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {AttributesModule} from '../attributes.module';
-import {BreadcrumbBComponent} from './breadcrumb.bcomponent';
+import {BreadcrumbBComponent, BreadcrumbBDirective} from './breadcrumb.bcomponent';
 import {LinkModule} from '../link/link.module';
+
+var directives = [
+    BreadcrumbBComponent,
+    BreadcrumbBDirective
+];
 
 @NgModule({
     imports: [BrowserModule, AttributesModule, LinkModule],
-    declarations: [BreadcrumbBComponent],
-    exports: [BreadcrumbBComponent]
+    declarations: directives,
+    exports: directives
 })
 export class BreadcrumbModule {}
