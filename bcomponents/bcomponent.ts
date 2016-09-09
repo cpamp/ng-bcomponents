cf023492eae3a0436cfbe60be8e83cfc6b6991b8
Clint Pamperin <c.pamperin21@gmail.com>
2016-09-08 22:32:19 -0500

    add: JSDocs

--- bcomponents/bcomponent.ts
+++ bcomponents/bcomponent.ts
@@ -43,13 +43,19 @@ export class BComponentAttributes {
 }
 
 export class BComponent {
-    // Inputs
+    /** id attribute */
     public id: string;
+    /** class attribute */
     public classes: string;
+    /** style attribute */
     public styles: string;
+    /** name attribute */
     public name: string;
+    /** aria attribute */
     public aria: string;
+    /** aria-labelledby attribute */
     public ariaBy: string;
+
     protected bcomponent: any = null;
 
     protected self = this;
@@ -121,6 +127,16 @@ export class BComponent {
         return value == null;
     }
 
+    /**
+     * Initialize the component attributes
+     * @param {string} id - Id attribute for the component
+     * @param {string} classes - Class attribute for the component
+     * @param {string} styles - Style attribute for the component
+     * @param {string} name - Name attribute for the component
+     * @param {string} aria - Aria attribute for the component
+     * @param {string} ariaBy - Aria-labelledby attribute for the component
+     * @returns {this} - Returns this object
+     */
     public InitializeAttributes = (id: string = "", classes: string = "", styles: string = "", name: string = "", aria: string = "", ariaBy: string = ""): this => {
         this.id = id;
         this.classes = classes;
@@ -132,54 +148,118 @@ export class BComponent {
         return this;
     }
 
+    /**
+     * Disable auto identifiers for BComponents
+     */
     public static disableAutoIdentifier = () => {
         BComponent.autoIdentifier = false;
     }
 
-    /** Animations */
+    /**
+     * Get the JQuery selector for the component
+     * @returns {JQuery} - JQuery selector for the element
+     */
     public getSelector = (): JQuery => {
         return $(IdentifierFactory.getSelector(this.id));
     }
 
-    /** Toggle / Hide / Show */
+    /**
+     * Toggle the component. Hide/show
+     * @param {string|number} duration - Duration of the Animations
+     * @param {Function?} callback - Callback on animation completion
+     * @returns {JQuery} - JQuery selector for the component
+     */
     public toggle = (duration: string | number = 400, callback?: Function): JQuery => {
         return this.getSelector().toggle(duration, callback);
     }
 
+    /**
+     * Hide the component
+     * @param {string|number} duration - Duration of the Animations
+     * @param {Function?} callback - Callback on animation completion
+     * @returns {JQuery} - JQuery selector for the component
+     */
     public hide = (duration: string | number = 400, callback?: Function): JQuery => {
         return this.getSelector().hide(duration, callback);
     }
 
+    /**
+     * Show the component
+     * @param {string|number} duration - Duration of the Animations
+     * @param {Function?} callback - Callback on animation completion
+     * @returns {JQuery} - JQuery selector for the component
+     */
     public show = (duration: string | number = 400, callback?: Function): JQuery => {
         return this.getSelector().show(duration, callback);
     }
 
-    /** FadeToggle / FadeIn / FadeOut / FadeTo */
+    /**
+     * Fade toggle the component
+     * @param {string|number} duration - Duration of the Animations
+     * @param {Function?} callback - Callback on animation completion
+     * @returns {JQuery} - JQuery selector for the component
+     */
     public fadeToggle = (duration: string | number = 400, callback?: Function): JQuery => {
         return this.getSelector().fadeToggle(duration, callback);
     }
 
+    /**
+     * Fade the component into view
+     * @param {string|number} duration - Duration of the Animations
+     * @param {Function?} callback - Callback on animation completion
+     * @returns {JQuery} - JQuery selector for the component
+     */
     public fadeIn = (duration: string | number = 400, callback?: Function): JQuery => {
         return this.getSelector().fadeIn(duration, callback);
     }
 
+    /**
+     * Fade the component out of view
+     * @param {string|number} duration - Duration of the Animations
+     * @param {Function?} callback - Callback on animation completion
+     * @returns {JQuery} - JQuery selector for the component
+     */
     public fadeOut = (duration: string | number = 400, callback?: Function): JQuery => {
         return this.getSelector().fadeOut(duration, callback);
     }
 
+    /**
+     * Fade the component to an opacity
+     * @param {string|number} duration - Duration of the Animations
+     * @param {number} opacity - Opacity to fade to
+     * @param {Function?} callback - Callback on animation completion
+     * @returns {JQuery} - JQuery selector for the component
+     */
     public fadeTo = (duration: string | number = 400, opacity: number = 0, callback?: Function): JQuery => {
         return this.getSelector().fadeTo(duration, opacity, callback);
     }
 
-    /** SlideToggle, SlideDown, SlideUp */
+    /**
+     * Slide toggle the element
+     * @param {string|number} duration - Duration of the Animations
+     * @param {Function?} callback - Callback on animation completion
+     * @returns {JQuery} - JQuery selector for the component
+     */
     public slideToggle = (duration: string | number = 400, callback?: Function): JQuery => {
         return this.getSelector().slideToggle(duration, callback);
     }
 
+    /**
+     * Slide the component into view
+     * @param {string|number} duration - Duration of the Animations
+     * @param {Function?} callback - Callback on animation completion
+     * @returns {JQuery} - JQuery selector for the component
+     */
     public slideDown = (duration: string | number = 400, callback?: Function): JQuery => {
         return this.getSelector().slideDown(duration, callback);
     }
 
+    /**
+     * Slide the component out of view
+     * @param {string|number} duration - Duration of the Animations
+     * @param {Function?} callback - Callback on animation completion
+     * @returns {JQuery} - JQuery selector for the component
+     */
     public slideUp = (duration: string | number = 400, callback?: Function): JQuery => {
         return this.getSelector().slideUp(duration, callback);
     }
